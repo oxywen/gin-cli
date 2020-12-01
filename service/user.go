78b0f80c8a9681ed2a87f800aaf7a295f1c8b997
WenRanran <1695687756@qq.com>
2020-12-01 20:54:39 +0800

    更改命名，添加测试路由

--- service/user.go
+++ service/user.go
@@ -6,8 +6,5 @@ import (
 )
 
 func Hello(name string) string {
-	if name == "" {
-		return "嘿，亲爱的，你还没告诉我你的名字呢"
-	}
 	return fmt.Sprintf("hello %s, %s", name, time.Now().Format("2006-01-02 15:04:05"))
 }
